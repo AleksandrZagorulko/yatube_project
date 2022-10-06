adeecfe57036bbd0f2863e71940b4d190e2df99b
Александр Загорулько <alekszagorulcko@yandex.ru>
2022-10-06 16:51:55 +0300

    views.py added and changed

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    # Main page.
+    return HttpResponse('Главная страница лучшей в мире соцсети yatube')
+
+
+def group_posts(request):
+    # Group posts page.
+    return HttpResponse('Гениальные изречения, явления и тп наших пользователей из группировок')
